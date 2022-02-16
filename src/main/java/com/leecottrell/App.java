52ca9de6f252c70f622d1540aedbe6a409a3248f
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-02-16 09:53:10 -0500

    Lee

---
+++ src/main/java/com/leecottrell/App.java
@@ -0,0 +1,17 @@
+package com.leecottrell;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static void Lee(){
+        System.out.println("Lee");
+    }
+}
